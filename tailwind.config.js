2c081b8d83367bf5b0badea20ba5469712b93ac6
ScriptnSam <oluwafemisam40@gmail.com>
2024-04-17 14:44:47 +0100

    halfway done with navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./shop.html", "./index.html", "./contact.html"],
+  content: ["./**/*.html"],
   theme: {
     extend: {
       fontFamily: {
