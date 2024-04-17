b55ad09ecf1cd3d15d5b8ffdeb211f03e14cc877
ScriptnSam <oluwafemisam40@gmail.com>
2024-04-17 09:07:44 +0100

    firsrt commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./shop.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'jost': ['Jost', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
